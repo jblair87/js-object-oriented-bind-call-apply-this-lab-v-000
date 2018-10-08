622a6d32d57f373f1828f429284bb9d6be0fda1a
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-08 21:13:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ function justInvoke(){}
 function setThisWithCall(fn, thisValue, arg){
   justInvoke.call(fn, thisValue, arg);
   this.age = age;
-  
+  this.hairColor = hairColor;
 }
+console.log(new setThisWithCall('Bob', 5).name);
+
 function setThisWithApply(){}
 function returnNewFunctionOf(){}
