c4d453ec966bc181a8764b012d3b4b89ba9e605a
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-08 21:23:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ return fn.call(thisValue, arg);
 }
 
 function setThisWithApply(fn, thisValue, args){
-  
+  return fn.apply(thisValue, args);
 }
 
 function returnNewFunctionOf(functionToBeCopied, thisValue){
